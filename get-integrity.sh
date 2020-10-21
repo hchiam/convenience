028fc77097366ab06759b41ec18c326168e07dba
hchiam <hchiam@users.noreply.github.com>
2020-10-20 23:22:54 -0400

    actually working example

--- get-integrity.sh
+++ get-integrity.sh
@@ -1,6 +1,7 @@
-function sha() { # usage: sha FILENAME.js
+function sha() {
   tempShaOutput=$(cat $1 | openssl dgst -sha384 -binary | openssl base64 -A)
   echo sha384-$tempShaOutput
 }
 
-# sha someFileName.js
+# Usage:
+# source get-integrity.sh; sha someFileName.js
