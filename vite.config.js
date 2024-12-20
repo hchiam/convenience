71e870ff62feb8b22417cb6b3cd673735fef4ded
hchiam <hchiam@users.noreply.github.com>
2024-12-19 19:35:17 -0700

    making vite local demo work too

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+// for vite to work and open demo.html, you'll have to yarn add vite --dev --exact
+import { defineConfig } from "vite";
+export default defineConfig({
+  server: {
+    open: "demo.html",
+  },
+});
